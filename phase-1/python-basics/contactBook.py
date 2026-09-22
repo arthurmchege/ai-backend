577d6c66a9c641b6fbb68237bc7798370ba54952
Arthur Mulunda <arthurmulunda941@gmail.com>
2026-09-22 17:48:04 +0300

    feat: created a new function to add new contacts

--- phase-1/python-basics/contactBook.py
+++ phase-1/python-basics/contactBook.py
@@ -1,42 +1,43 @@
 import json
 
-class Contact:
+class Contact():
     def __init__(self, name, phone, email):
         self.name = name
         self.phone = phone
         self.email = email
 
-    def address(self):
-        return f"Hello, here are my contact details: {self.name}, {self.phone}, {self.email}."
-
-arthur = Contact("Arthur", "1122334455", "arthur@example.com")
-print(arthur.address())
-
-def newContact(name, phone, email):
-    try:
-        contacts = {
-            "name": name,
-            "phone": phone,
-            "email": email
+    def newContact(self, name, phone, email):
+        try:
+            with open("contacts.json", "r") as f:
+                loaded_data = json.load(f)
+        except FileNotFoundError:
+            existing_contacts = {}
+        contact = Contact(name, phone, email)
+        contact_data = {
+            "name": contact.name,
+            "phone": contact.phone,
+            "email": contact.email
         }
+        with open("contacts.json", "w") as f:
+            json.dump(contact_data, f)
+            return f"Contact {contact.name} saved successfully."
 
-        with open("phone_book.json", "w") as f:
-            json.dump(contacts, f)
+        def findContact(self, name):
+            try:
+                with open("contacts.json", "r") as f:
+                    loaded_data = json.load(f)
+                    if  findContact.name == {name}:
+                        return f"Contact {name} found: {loaded_data}"
+            except:
+                print("Contact not found")
+            print(loaded_data)
 
-        with open("phone_book.json", "r") as f:
-            loaded_contacts = json.load(f)
-        print(loaded_contacts)
-        return contacts
-    except KeyError:
-        print("Error: Missing contact information.")
 
-def findContact(name, phone, email):
-    try:
-        with open("phone_book.json", "r") as f:
-            loaded_contacts = json.load(f)
-            if loaded_contacts["name"] == name and loaded_contacts["email"] == email and loaded_contacts["phone"] == phone:
-                return f"Contact found: {loaded_contacts['name']}, {loaded_contacts['phone']}, {loaded_contacts['email']}."
-            else:
-                return f"Contact not found"
-    except KeyError:
-      print("Error: MIssing Contact Information")
+name = input("Enter your name: ")
+phone = input("Enter your phone number: ")
+email = input("Enter your email: ")
+
+add_contact = Contact(name, phone, email)
+print(add_contact.newContact(name, phone, email))
+
+
